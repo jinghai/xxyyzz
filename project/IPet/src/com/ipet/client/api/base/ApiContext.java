7ad31b4ff889eddf12152ff5da7bf572a146ff06
jinghai <jinghai.xiao@gmail.com>
2014-01-15 21:18:43 +0800

    8080

--- project/IPet/src/com/ipet/client/api/base/ApiContext.java
+++ project/IPet/src/com/ipet/client/api/base/ApiContext.java
@@ -20,7 +20,7 @@ public class ApiContext {
 
     private String currUserId;
 
-    public static final String API_URL_BASE = "http://localhost:8084/server/api/v1/";
+    public static final String API_URL_BASE = "http://localhost:8080/server/api/v1/";
 
     private static ApiContext instance;
 
