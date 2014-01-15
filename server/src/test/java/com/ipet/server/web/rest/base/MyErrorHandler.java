0a6159bbe4e6d7e428ecfa1ab703da2a3de0ba98
jinghai <jinghai.xiao@gmail.com>
2014-01-15 13:56:59 +0800

    ApiExceptionHandler

--- server/src/test/java/com/ipet/server/web/rest/base/MyErrorHandler.java
+++ server/src/test/java/com/ipet/server/web/rest/base/MyErrorHandler.java
@@ -1,11 +1,16 @@
 package com.ipet.server.web.rest.base;
 
-
 import java.io.IOException;
-import org.apache.commons.io.IOUtils;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestClientException;
 
 /*
  * To change this template, choose Tools | Templates,
@@ -17,16 +22,48 @@ import org.springframework.web.client.ResponseErrorHandler;
  */
 public class MyErrorHandler implements ResponseErrorHandler {
 
-    private final ResponseErrorHandler errorHandler = new DefaultResponseErrorHandler();
-
+    //private final ResponseErrorHandler errorHandler = new DefaultResponseErrorHandler();
     @Override
     public boolean hasError(ClientHttpResponse response) throws IOException {
-        return errorHandler.hasError(response);
+        return hasError(response.getStatusCode());
+        //return errorHandler.hasError(response);
+    }
+
+    protected boolean hasError(HttpStatus statusCode) {
+        return (statusCode.series() == HttpStatus.Series.CLIENT_ERROR
+                || statusCode.series() == HttpStatus.Series.SERVER_ERROR);
     }
 
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
-        String theString = IOUtils.toString(response.getBody(), "UTF-8");
-        throw new RuntimeException("Error:" + theString);
+        //String theString = IOUtils.toString(response.getBody(), "UTF-8");
+        HttpStatus statusCode = response.getStatusCode();
+        MediaType contentType = response.getHeaders().getContentType();
+        Charset charset = contentType != null ? contentType.getCharSet() : null;
+        byte[] body = getResponseBody(response);
+        String str = new String(body, charset);
+        /*
+         switch (statusCode.series()) {
+         case CLIENT_ERROR:
+         throw new HttpClientErrorException(statusCode, response.getStatusText(), body, charset);
+         case SERVER_ERROR:
+         throw new HttpServerErrorException(statusCode, response.getStatusText(), body, charset);
+         default:
+         throw new RestClientException("Unknown status code [" + statusCode + "]");
+         }
+         */
+        throw new RuntimeException("Error:" + str);
+    }
+
+    private byte[] getResponseBody(ClientHttpResponse response) {
+        try {
+            InputStream responseBody = response.getBody();
+            if (responseBody != null) {
+                return FileCopyUtils.copyToByteArray(responseBody);
+            }
+        } catch (IOException ex) {
+            // ignore
+        }
+        return new byte[0];
     }
 }
--- server/src/test/java/com/ipet/server/web/rest/v1/TestUserRestController.java
+++ server/src/test/java/com/ipet/server/web/rest/v1/TestUserRestController.java
@@ -31,14 +31,14 @@ public class TestUserRestController extends BaseTest {
 
     public static final String baseUrl = "http://localhost:8084/server/api/v1/user";
 
-    @Test
+    //@Test
     public void getOneUser() {
         //使用URI模板方式,参数也可使用Map<String, Object> urlVariables = new HashMap< String, Object>();
         User user = restTemplate.getForObject(baseUrl + "/{id}", User.class, "1");
         logger.info(user.toString());
     }
 
-    @Test
+    // @Test
     public void getUsers() {
         //MultiValueMap<String, List<Long>> body = new LinkedMultiValueMap<String, List<Long>>();
         //Map<String, Object> urlVariables = new HashMap< String, Object>();
@@ -48,7 +48,7 @@ public class TestUserRestController extends BaseTest {
         logger.info(list.toString());
     }
 
-    @Test
+    //@Test
     public void getUsersForOne() {
         //MultiValueMap<String, List<Long>> body = new LinkedMultiValueMap<String, List<Long>>();
         //Map<String, Object> urlVariables = new HashMap< String, Object>();
@@ -58,7 +58,7 @@ public class TestUserRestController extends BaseTest {
         logger.info(list.toString());
     }
 
-    @Test
+    //@Test
     public void updateInfo() {
         //直接发送实体，自动转为Json格式提交到服务器
         User u = new User();
@@ -69,7 +69,7 @@ public class TestUserRestController extends BaseTest {
         logger.info(u.toString());
     }
 
-    @Test
+    //@Test
     public void uploadForHttpEntity() {
 
         String url = baseUrl + "/uploadAvatar";
@@ -89,7 +89,7 @@ public class TestUserRestController extends BaseTest {
         //restTemplate.postForObject(url, body, String.class);
     }
 
-    @Test
+    //@Test
     public void uploadFor4M() {
 
         logger.debug(ClassLoader.getSystemResource("4M.JPG").getPath());
@@ -104,7 +104,7 @@ public class TestUserRestController extends BaseTest {
 
     }
 
-    //@Test
+    @Test
     //测试超过5M的文件
     public void uploadFor5M() {
 
