cd071685a9b99652d9d6da625c01789a261ce5d3
jinghai <jinghai.xiao@gmail.com>
2014-02-28 03:02:33 +0800

    提交部分脚步

---
+++ server/shells/install.sh
@@ -0,0 +1,151 @@
+#!/bin/sh
+#
+# 
+# 
+
+#eth0(内网)若需限制某网卡的端口，请参考以下语句，,eth1(电信)，eth2(连通)只开放80端口，
+#iptables -F
+#iptables -A INPUT -i eth1 -p tcp --dport 80 -j ACCEPT
+#iptables -A INPUT -i eth1 -p tcp -j REJECT
+#iptables -A INPUT -i eth2 -p tcp --dport 80 -j ACCEPT
+#iptables -A INPUT -i eth2 -p tcp -j REJECT
+#service iptables save
+#service iptables restart
+
+#####初始化
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+if [ ! -f "/etc/profile.old" ]; then 
+    \cp -a  /etc/profile /etc/profile.bak
+fi
+if [ ! -f "/etc/rc.d/rc.local" ]; then 
+    \cp -a  /etc/profile /etc/rc.d/rc.local.bak
+fi
+if [ ! -f "/root/.bash_profile" ]; then 
+    \cp -a  /etc/profile /root/.bash_profile.bak
+fi
+chkconfig iptables off
+service iptables stop
+yum -y remove java java-1.4.2-gcj-compat-1.4.2.0-40jpp.115
+grep -q "ulimit -n 102400" /etc/rc.d/rc.local &&{
+	echo "ulimit has been setted."
+}||{
+	echo "ulimit -n 102400" >> /etc/rc.d/rc.local
+}
+chmod +x /etc/rc.d/rc.local
+grep -q "ulimit -n 102400" /root/.bash_profile &&{
+	echo "ulimit has been setted."
+}||{
+	echo "ulimit -n 102400" >> /root/.bash_profile
+}
+chmod +x /root/.bash_profile
+
+#####安装工具
+yum install wget git  -y
+
+
+#####安装JDK
+if [ ! -f "/download/jdk-7u15-linux-x64.tar.gz" ]; then 
+    wget -P /download --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/7u15-b03/jdk-7u15-linux-x64.tar.gz" 
+fi
+if [ ! -d "/opt/" ]; then 
+    mkdir -p /opt
+fi
+echo "正在安装JDK..."
+tar zxf /download/jdk-7u15-linux-x64.tar.gz -C /opt
+grep -q "export JAVA_HOME" /etc/profile &&{
+	echo "JDK config exits."
+}||{
+	echo "" >>/etc/profile
+	echo "export JAVA_HOME=/opt/jdk1.7.0_15 #AUTO_GEN_JDK" >>/etc/profile
+	echo "export CLASSPATH=.:\$JAVA_HOME/lib:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar #AUTO_GEN_JDK" >>/etc/profile  
+	echo "export PATH=\$PATH:\$JAVA_HOME/bin #AUTO_GEN_JDK" >>/etc/profile
+	echo "export JRE_HOME=\$JAVA_HOME/jre #AUTO_GEN_JDK" >>/etc/profile
+}
+sleep 1
+source /etc/profile
+
+#####安装Maven
+if [ ! -f "/download/apache-maven-3.2.1-bin.tar.gz" ]; then 
+    wget -P /download  "http://mirror.bit.edu.cn/apache/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz" 
+fi
+echo "正在安装Maven..."
+tar zxf /download/apache-maven-3.2.1-bin.tar.gz -C /opt
+grep -q "export M2_HOME" /etc/profile &&{
+	echo "Maven config exits."
+}||{
+	echo "" >>/etc/profile
+	echo "export M2_HOME=/opt/apache-maven-3.2.1 #AUTO_GEN_Maven" >>/etc/profile
+	echo "export PATH=\$PATH:\$M2_HOME/bin #AUTO_GEN_Maven" >>/etc/profile
+}
+sleep 1
+source /etc/profile
+
+#####安装MySQL
+if [ ! -f "/download/mysql-5.6.16-linux-glibc2.5-x86_64.tar.gz" ]; then 
+    wget -P /download  "http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.16-linux-glibc2.5-x86_64.tar.gz" 
+fi
+echo "正在安装Mysql..."
+tar zxf /download/mysql-5.6.16-linux-glibc2.5-x86_64.tar.gz -C /opt
+
+installPath=/opt/mysql-5.6.16-linux-glibc2.5-x86_64
+dataPath=/home/data/mysql
+sockFile=/tmp/mysql.sock
+
+systemGroupName=mysql
+systemUserName=mysql
+dataBackPath=/home/backup/mysql
+password=itserver
+port=3306
+serviceName=mysqld
+remoteUser=admin
+
+mkdir -p ${installPath}
+groupadd ${systemGroupName}
+useradd -r -g ${systemGroupName} ${systemUserName}
+
+
+mkdir -p ${dataPath}
+mkdir -p ${dataBackPath}
+chown -R ${systemUserName} ${installPath}
+chgrp -R ${systemGroupName} ${installPath}
+chown -R ${systemUserName} ${dataPath}
+chgrp -R ${systemGroupName} ${dataPath}
+chown -R ${systemUserName} ${dataBackPath}
+chgrp -R ${systemGroupName} ${dataBackPath}
+
+${installPath}/scripts/mysql_install_db --basedir=${installPath} --datadir=${dataPath} --user=${systemUserName} 
+#chown -R root $installPath
+#chown -R $systemUserName $dataPath
+
+\cp -a ./my.cnf /etc/my.cnf
+\cp -a ${installPath}/support-files/mysql.server ./mysql.server.tmp
+sed -i "/^basedir=/c\basedir=$installPath" ./mysql.server.tmp
+sed -i "/^datadir=/c\datadir=$dataPath" ./mysql.server.tmp
+\cp ./mysql.server.tmp /etc/init.d/${serviceName}
+chmod +x /etc/init.d/${serviceName}
+chkconfig --add ${serviceName}
+chkconfig ${serviceName} on
+service ${serviceName} start
+
+grep -q "AUTO_GEN_MYSQL" /etc/profile &&{
+	echo "Maven config exits."
+}||{
+	echo "" >>/etc/profile
+	echo "export PATH=\$PATH:/opt/mysql-5.6.16-linux-glibc2.5-x86_64/bin #AUTO_GEN_MYSQL" >>/etc/profile  
+}
+sleep 1
+source /etc/profile
+
+${installPath}/bin/mysqladmin -S${sockFile} -uroot password "${password}"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password} -e "GRANT all on *.* to '$remoteUser'@'%' identified by \"${password}\";"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password} -e "GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%' identified by 'repl';"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password} -e "GRANT Select ON *.* TO 'readonly'@'localhost' identified by 'readonly';"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password}  -e "delete from mysql.user where user='';"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password}  -e "delete from mysql.user where password='';"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password}  -e "flush privileges;"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password}  -e "drop database if exists ipet;create database ipet;"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password} -e "INSTALL PLUGIN rpl_semi_sync_master SONAME 'semisync_master.so';"
+${installPath}/bin/mysql -S${sockFile} -uroot -p${password} -e "INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';"
+service ${serviceName} restart
+
+echo "===========install ok===========" 
---
+++ server/shells/uninstall.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# 
+# 
+
+
+yum -y remove wget git
+
+sed "/ulimit -n 102400/d" /etc/rc.d/rc.local > /tmp/t.t
+\cp /tmp/t.t /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
+
+sed "/ulimit -n 102400/d" /root/.bash_profile > /tmp/t.t
+\cp /tmp/t.t /root/.bash_profile
+chmod +x /root/.bash_profile
+
+#####卸载JDK
+sed "/AUTO_GEN_JDK/d" /etc/profile > /tmp/t.t
+\cp /tmp/t.t /etc/profile
+sleep 1
+source /etc/profile
+rm -rf /opt/jdk1.7.0_15
+
+#####卸载Maven
+sed "/AUTO_GEN_Maven/d" /etc/profile > /tmp/t.t
+\cp /tmp/t.t /etc/profile
+sleep 1
+source /etc/profile
+rm -rf /opt/apache-maven-3.2.1
+
+#####卸载MySQL
+installPath=/opt/mysql-5.6.16-linux-glibc2.5-x86_64
+serviceName=mysqld
+service ${serviceName} stop
+chkconfig --del ${serviceName}
+rm -rf /etc/init.d/${serviceName}
+rm -rf ${installPath}
+sed "/AUTO_GEN_MYSQL/d" /etc/profile > /tmp/t.t
+\cp /tmp/t.t /etc/profile
+sleep 1
+source /etc/profile
+echo "===========clear ok===========" 
