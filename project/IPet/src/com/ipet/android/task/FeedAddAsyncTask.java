e8882d17885047501d90b30b7501287579a5a416
jinghai <jinghai.xiao@gmail.com>
2014-03-13 00:12:51 +0800

    解决不能连续上传的问题

--- project/IPet/src/com/ipet/android/task/FeedAddAsyncTask.java
+++ project/IPet/src/com/ipet/android/task/FeedAddAsyncTask.java
@@ -14,46 +14,47 @@ import com.ipet.android.sdk.domain.IpetUser;
 import com.ipet.android.ui.MainHomeFragment;
 
 public class FeedAddAsyncTask extends AsyncTask<String, String, IpetPhoto> {
-	private final Uri uri;
-	private final MainHomeFragment fragment;
-	private final File picture;
 
-	public FeedAddAsyncTask(MainHomeFragment fragment, File picture, Uri uri) {
-		this.uri = uri;
-		this.fragment = fragment;
-		this.picture = picture;
-	}
+    private final Uri uri;
+    private final MainHomeFragment fragment;
+    private final File picture;
 
-	@Override
-	protected IpetPhoto doInBackground(String... params) {
-		// TODO Auto-generated method stub
-		Log.i("uploadFile", "pictrue-->" + picture);
-		IpetPhoto ipetPhoto = null;
-		try {
-			Thread.sleep(500);
-			MyApp application = (MyApp) this.fragment.getActivity().getApplication();
-			IpetUser user = application.getUser();
-			String path = picture.getPath();
-			Log.i("uploadFile", "path-->" + picture);
-			ipetPhoto = application.getApi().getPhotoApi().publish("", path);
-			Log.i("uploadFile", "SmallURL-->" + ipetPhoto.getSmallURL());
-			ipetPhoto.setAvatar48(user.getAvatar48());
-			ipetPhoto.setUserName(user.getDisplayName());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public FeedAddAsyncTask(MainHomeFragment fragment, File picture, Uri uri) {
+        this.uri = uri;
+        this.fragment = fragment;
+        this.picture = picture;
+    }
 
-		return ipetPhoto;
-	}
+    @Override
+    protected IpetPhoto doInBackground(String... params) {
+        // TODO Auto-generated method stub
+        Log.i("uploadFile", "pictrue-->" + picture);
+        IpetPhoto ipetPhoto = null;
+        try {
+            Thread.sleep(500);
+            MyApp application = (MyApp) this.fragment.getActivity().getApplication();
+            IpetUser user = application.getUser();
+            String path = picture.getPath();
+            Log.i("uploadFile", "path-->" + picture);
+            ipetPhoto = application.getApi().getPhotoApi().publish("", path);
+            Log.i("uploadFile", "SmallURL-->" + ipetPhoto.getSmallURL());
+            //ipetPhoto.setAvatar48(user.getAvatar48());
+            //ipetPhoto.setUserName(user.getDisplayName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	@Override
-	protected void onPostExecute(IpetPhoto ipetPhoto) {
-		if (ipetPhoto == null) {
-			return;
-		}
+        return ipetPhoto;
+    }
 
-		List<IpetPhoto> list = new ArrayList<IpetPhoto>(0);
-		list.add(ipetPhoto);
-		this.fragment.getAdapter().prependList(list);
-	}
+    @Override
+    protected void onPostExecute(IpetPhoto ipetPhoto) {
+        if (ipetPhoto == null) {
+            return;
+        }
+
+        List<IpetPhoto> list = new ArrayList<IpetPhoto>(0);
+        list.add(ipetPhoto);
+        this.fragment.getAdapter().prependList(list);
+    }
 }
