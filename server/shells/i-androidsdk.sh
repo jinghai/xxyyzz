b69d6671ba7897b3088250c68cd41d431b10ff19
jinghai <jinghai.xiao@gmail.com>
2014-03-14 02:05:24 +0800

    shell

--- server/shells/i-androidsdk.sh
+++ server/shells/i-androidsdk.sh
@@ -4,30 +4,23 @@
 #安装JDK
 
 echo "正在安装Android SDK..."
-if [ ! -f "/download/android-sdk_r22.6-linux.tgz" ]; then
-    wget -P /download --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://dl.google.com/android/android-sdk_r22.6-linux.tgz"
+if [ ! -f "/download/adt-bundle-linux-x86_64-20131030.zip" ]; then
+    wget -P /download --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://dl.google.com/android/adt/adt-bundle-linux-x86_64-20131030.zip"
 fi
 if [ ! -d "/opt/" ]; then
     mkdir -p /opt
 fi
-tar zxf /download/android-sdk_r22.6-linux.tgz -C /opt
+tar zxf /download/adt-bundle-linux-x86_64-20131030.zip -C /opt
+unzip  /download/adt-bundle-linux-x86_64-20131030.zip -d /opt
 
-Android_SDK_Home=/opt/android-sdk-linux
-
-grep -q "AUTO_Android_SDK" /etc/profile &&{
-	echo "Android SDK config exits."
-}||{
-	echo "#AUTO_Android_SDK" >>/etc/profile
-	echo "export PATH=\$PATH:\$Android_SDK_Home/tools #AUTO_Android_SDK" >>/etc/profile
-}
-sleep 1
-. /etc/profile
-
-android -h
+Android_SDK_Home=/opt/adt-bundle-linux-x86_64-20131030
 
 
-#/opt/android-sdk-linux/tools/android list targets			#检查已安装的SDK版本
-#/opt/android-sdk-linux/tools/android list sdk			#列出所有SDK
-#/opt/android-sdk-linux/tools/android update sdk -t 1,2,4 -u 		#更新SDK
+/opt/adt-bundle-linux-x86_64-20131030/sdk/tools/android list targets
+
+#/opt/adt-bundle-linux-x86_64-20131030/sdk/tools/android list targets			#检查已安装的SDK版本
+#/opt/adt-bundle-linux-x86_64-20131030/sdk/tools/android list sdk				#列出所有SDK
+#/opt/adt-bundle-linux-x86_64-20131030/sdk/tools/android update sdk -t 1,2,4 -u 		#更新SDK
+#ant -f /src/xxyyzz/project/IPet clean release
 
 echo "===========Android SDK==========="
