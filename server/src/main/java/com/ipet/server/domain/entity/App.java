465e1401a88e0c1b0f09738f69e563da9bebc9f4
jinghai <jinghai.xiao@gmail.com>
2014-03-20 00:23:22 +0800

    应用实例增加升级相关字段

--- server/src/main/java/com/ipet/server/domain/entity/App.java
+++ server/src/main/java/com/ipet/server/domain/entity/App.java
@@ -38,6 +38,18 @@ public class App extends IdEntity implements Serializable {
 
     private String userId;
 
+    private String userName;
+
+    //数值版本号,用于版本更新比较
+    private Integer versionCode;
+
+    //字符串版本号，呈现给用户界面
+    private String versionName;
+
+    //app下载地址（完整路径）
+    @Column(length = 500)
+    private String appDownloadUrl;
+
     /**
      * @return the name
      */
@@ -106,6 +118,62 @@ public class App extends IdEntity implements Serializable {
      */
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the versionCode
+     */
+    public Integer getVersionCode() {
+        return versionCode;
+    }
+
+    /**
+     * @param versionCode the versionCode to set
+     */
+    public void setVersionCode(Integer versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    /**
+     * @return the versionName
+     */
+    public String getVersionName() {
+        return versionName;
+    }
+
+    /**
+     * @param versionName the versionName to set
+     */
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    /**
+     * @return the appDownloadUrl
+     */
+    public String getAppDownloadUrl() {
+        return appDownloadUrl;
+    }
+
+    /**
+     * @param appDownloadUrl the appDownloadUrl to set
+     */
+    public void setAppDownloadUrl(String appDownloadUrl) {
+        this.appDownloadUrl = appDownloadUrl;
     }
 
 }
