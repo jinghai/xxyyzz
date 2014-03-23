87de62ac83c48473ab6a7c6f2ff3779cc49e301c
罗灿锋 <luocanfeng@gmail.com>
2014-03-23 09:45:20 +0800

    屏蔽jetty的一个debug错误信息

--- server/src/test/java/com/ipet/client/api/AppApiTest.java
+++ server/src/test/java/com/ipet/client/api/AppApiTest.java
@@ -1,21 +1,22 @@
 package com.ipet.client.api;
 
-import com.ipet.client.api.domain.IpetAppUpdate;
-import com.ipet.client.api.impl.IpetApiImpl;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ipet.client.api.domain.IpetAppUpdate;
+import com.ipet.client.api.impl.IpetApiImpl;
+
 public class AppApiTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(AppApiTest.class);
-    private final AppApi apApi = IpetApiImpl.getInstance("ipet", "ipet").getAppApi();
+	private static final Logger logger = LoggerFactory.getLogger(AppApiTest.class);
+	private final AppApi apApi = IpetApiImpl.getInstance("ipet", "ipet").getAppApi();
 
-    @Test
-    public void testFeedbackAnonymous() {
-        IpetAppUpdate ret = apApi.checkAppVersion("ipet");
-        logger.debug(ToStringBuilder.reflectionToString(ret));
-    }
+	@Test
+	public void testCheckAppVersion() {
+		IpetAppUpdate ret = apApi.checkAppVersion("ipet");
+		logger.debug(ToStringBuilder.reflectionToString(ret));
+	}
 
 }
