2cee92767fcdd8fa22e0b33513b12943d59b23cc
luocanfeng <luocanfeng@gmail.com>
2014-03-28 16:16:40 +0800

    remove unused dependencies

--- server/src/main/java/com/ipet/server/repository/AppDao.java
+++ server/src/main/java/com/ipet/server/repository/AppDao.java
@@ -1,9 +1,11 @@
 package com.ipet.server.repository;
 
-import com.ipet.server.domain.entity.App;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ipet.server.domain.entity.App;
 
 public interface AppDao extends JpaRepository<App, String> {
 
-    public App findByAppKey(String appKey);
+	public App findByAppKey(String appKey);
+
 }
