b5cbf7bf1c13580c1a2f273be10025bfa2889223
jinghai <jinghai.xiao@gmail.com>
2014-04-09 23:50:28 +0800

    增加启动时检查网络，无可用网络连接则退出。

--- project/IPet/src/com/ipet/android/ui/SplashActivity.java
+++ project/IPet/src/com/ipet/android/ui/SplashActivity.java
@@ -4,91 +4,112 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ipet.R;
 import com.ipet.android.Constant;
 import com.ipet.android.MyApp;
 import com.ipet.android.task.SplashLoginAsyncTask;
+import com.ipet.android.ui.manager.ActivityManager;
 import com.ipet.android.ui.manager.LoginManager;
 import com.ipet.android.ui.manager.UserManager;
 import com.ipet.android.ui.utils.AppUtils;
+import com.ipet.android.ui.utils.NetWorkUtils;
 
 public class SplashActivity extends Activity {
-	boolean isLogin = false;
-	private TextView version = null;
 
-	private static final int GO_MAIN = 1000;
-	private static final int GO_WELCOME = 1001;
+    boolean isLogin = false;
+    private TextView version = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
+    private static final int GO_MAIN = 1000;
+    private static final int GO_WELCOME = 1001;
 
-		init();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-	// 开始初始化
-	private void init() {
-		version = (TextView) this.findViewById(R.id.version_info);
-		String verStr = getResources().getString(R.string.app_version);
-		String v = String.format(verStr, AppUtils.getAppVersionName(this));
-		version.setText(v);
-		// 初始化模拟用户数据
-		MyApp application = (MyApp) this.getApplication();
-		if (application.getUser() == null) {
-			application.setUser(UserManager.getCurrentUser());
-		}
-		// 判断是否登录
-		isLogin = LoginManager.isLogin(SplashActivity.this);
-		// 判断程序与第几次运行，如果是第一次运行则跳转到引导界面，否则跳转到主界面
-		if (isLogin) {
-			// 使用Handler的postDelayed方法，3秒后执行跳转到MainActivity
-			// mHandler.sendEmptyMessageDelayed(GO_MAIN,
-			// Constant.SPLASH_DELAY_MILLIS);
-			// mHandler.sendEmptyMessageDelayed(GO_GUIDE,Constant.SPLASH_DELAY_MILLIS);
-			String[] u = LoginManager.getAccount(SplashActivity.this);
-			new SplashLoginAsyncTask(SplashActivity.this, u[0], u[1]).execute();
-		} else {
-			mHandler.sendEmptyMessageDelayed(GO_WELCOME, Constant.SPLASH_DELAY_MILLIS);
-		}
+        init();
+    }
 
-	}
+    // 开始初始化
+    private void init() {
+        version = (TextView) this.findViewById(R.id.version_info);
+        String verStr = getResources().getString(R.string.app_version);
+        String v = String.format(verStr, AppUtils.getAppVersionName(this));
+        version.setText(v);
+        // 初始化模拟用户数据
+        MyApp application = (MyApp) this.getApplication();
+        if (application.getUser() == null) {
+            application.setUser(UserManager.getCurrentUser());
+        }
+        //判断网络是否可用
+        boolean isNetworkConnected = NetWorkUtils.isNetworkConnected(this);
 
-	private final Handler mHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case GO_MAIN:
-				goMain();
-				break;
-			case GO_WELCOME:
-				goWelcome();
-				break;
-			}
+        // 判断是否登录
+        isLogin = LoginManager.isLogin(SplashActivity.this);
 
-			super.handleMessage(msg);
-		}
-	};
+        if (isNetworkConnected) {
+            // 判断程序与第几次运行，如果是第一次运行则跳转到引导界面，否则跳转到主界面
+            if (isLogin) {
+                String[] u = LoginManager.getAccount(SplashActivity.this);
+                new SplashLoginAsyncTask(SplashActivity.this, u[0], u[1]).execute();
+            } else {
+                mHandler.sendEmptyMessageDelayed(GO_WELCOME, Constant.SPLASH_DELAY_MILLIS);
+            }
+        } else {
+        	Toast.makeText(this, "没有发现可用网络,程序即将退出", 3).show();
+        	new Thread() {
+                @Override
+                public void run() {
+                	try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+                	SplashActivity.this.finish();
+                }
+            }.start();
 
-	// 转向主界面
-	public void goMain() {
-		Log.i("SPLASH", "to Main");
-		Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-		startActivity(intent);
-		finish();
+        }
 
-	}
+    }
 
-	// 转向引导界面
-	public void goWelcome() {
-		Log.i("SPLASH", "to Welcome");
-		Intent intent = new Intent(SplashActivity.this, WelcomeRegisterOrLoginActivity.class);
-		startActivity(intent);
-		finish();
-	}
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case GO_MAIN:
+                    goMain();
+                    break;
+                case GO_WELCOME:
+                    goWelcome();
+                    break;
+            }
+
+            super.handleMessage(msg);
+        }
+    };
+
+    // 转向主界面
+    public void goMain() {
+        Log.i("SPLASH", "to Main");
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    // 转向引导界面
+    public void goWelcome() {
+        Log.i("SPLASH", "to Welcome");
+        Intent intent = new Intent(SplashActivity.this, WelcomeRegisterOrLoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
 }
---
+++ project/IPet/src/com/ipet/android/ui/utils/NetWorkUtils.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ipet.android.ui.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ *
+ * @author yneos
+ *
+ * 参考：http://www.jb51.net/article/32920.htm
+ */
+public class NetWorkUtils {
+
+    //判断是否有网络连接 
+    public static boolean isNetworkConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null) {
+                return mNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+
+    //判断WIFI网络是否可用 
+    public static boolean isWifiConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mWiFiNetworkInfo = mConnectivityManager
+                    .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            if (mWiFiNetworkInfo != null) {
+                return mWiFiNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+
+    //判断MOBILE网络是否可用
+    public static boolean isMobileConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mMobileNetworkInfo = mConnectivityManager
+                    .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            if (mMobileNetworkInfo != null) {
+                return mMobileNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+
+    //获取当前网络连接的类型信息
+    public static int getConnectedType(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null && mNetworkInfo.isAvailable()) {
+                return mNetworkInfo.getType();
+            }
+        }
+        return -1;
+    }
+}
