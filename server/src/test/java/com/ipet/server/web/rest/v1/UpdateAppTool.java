0bf2acce35cfd5837ed4df21ea1822d7569bff4a
jinghai <jinghai.xiao@gmail.com>
2014-04-10 22:38:52 +0800

    更改发布客户端工具类的发布地址

--- server/src/test/java/com/ipet/server/web/rest/v1/UpdateAppTool.java
+++ server/src/test/java/com/ipet/server/web/rest/v1/UpdateAppTool.java
@@ -11,35 +11,33 @@ import com.ipet.server.domain.entity.App;
 import com.ipet.server.web.rest.base.BaseTest;
 
 /**
- * 
+ *
  * @author xiaojinghai
  */
 public class UpdateAppTool extends BaseTest {
 
-	public static final String baseUrl = "http://localhost:8080/server/api/v1/app";
+    public static final String baseUrl = "http://localhost:8080/server/api/v1/app";
+    //public static final String baseUrl = "http://api.ipetty.net/api/v1/app";
 
-	// public static final String baseUrl =
-	// "http://jinghai.imblog.in:8080/server/api/v1/app";
+    @Before
+    public void setUp() throws Exception {
+        JettyServer.start();
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		JettyServer.start();
-	}
+    @After
+    public void tearDown() throws Exception {
+        JettyServer.stop();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		JettyServer.stop();
-	}
-
-	@Test
-	public void update() {
-		// 发送MultiValueMap参数
-		MultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
-		request.add("appKey", "ipet");
-		request.add("versionCode", "5");
-		request.add("versionName", "0.5");
-		request.add("downloadUrl", "http://jinghai.imblog.in:8080/server/files/update/android/IPet-release.apk");
-		restTemplate.postForObject(baseUrl + "/update", request, App.class);
-	}
+    @Test
+    public void update() {
+        // 发送MultiValueMap参数
+        MultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
+        request.add("appKey", "ipet");
+        request.add("versionCode", "6");
+        request.add("versionName", "0.6");
+        request.add("downloadUrl", "http://api.ipetty.net/files/update/android/IPet-release.apk");
+        restTemplate.postForObject(baseUrl + "/update", request, App.class);
+    }
 
 }
